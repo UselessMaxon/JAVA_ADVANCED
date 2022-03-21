bbdd7a13aa7fd375c01fd7e221c8cf779bade1c2
UselessMaxon <natasha.maxsim@yandex.ru>
2022-03-22 00:38:20 +0400

    Создал второй файл

---
+++ src/main/java/Calc_2.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Calc_2 {
+
+    /**
+     * Simple calculator for four types operation: "+" , "-" , "*" , "/"
+     * with console output
+     * @author Maks Chistyakov
+     * @return void
+     */
+
+    public byte questProgram() {
+
+        Scanner in = new Scanner(System.in);
+        byte questProgram = in.nextByte();
+        return questProgram;
+    }
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            Double X,Y;
+            String operation;
+            System.out.println("Введите число №1");
+            X = in.nextDouble();
+            System.out.println("Введите число №2");
+            Y = in.nextDouble();
+            in.nextLine(); // Очистка сканера
+
+            System.out.println("Введите одну из операций: ‘+’, ‘-’, ‘*’ или ‘/’.");
+            operation = in.nextLine();
+            switch (operation) {
+                case "+": System.out.printf("%.4f", X+Y);
+                    break;
+                case "-": System.out.printf("%.4f", X-Y);
+                    break;
+                case "*": System.out.printf("%.4f", X*Y);
+                    break;
+                case "/":
+                    if (Y == 0) { // Проверка деления на 0
+                        System.err.println("На 0 делить нельзя!");
+                    }
+                    else {
+                        System.out.printf("%.4f", X/Y);
+                    }
+                    break;
+                default:
+                    System.err.println("Введите корректную операцию"); // Вывод в консоль, если символ введен некорректный
+                    break;
+            }
+
+            in.close();
+
+        }}
