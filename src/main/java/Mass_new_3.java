937427a9fc9c6c22a2cd831800cb42c4abff4db8
UselessMaxon <natasha.maxsim@yandex.ru>
2022-04-03 12:57:53 +0400

    3 задание исправление и без комментов

--- src/main/java/Mass_new_3.java
+++ src/main/java/Mass_new_3.java
@@ -11,34 +11,53 @@ public class Mass_new_3 {
         int numMax = 0;
         int counter = 0;
 
-        // перебор всех данных в массиве и заполнение целыми рандомными числами от - 20 до + 20
         while (counter < mass.length) {
             int rnd = (int) (Math.random() * 21) - 10;
             mass[counter] = rnd;
 
             if (mass[counter] > max) {
-                max = mass[counter];   // Максимальное число записывается в max, а индекс в numMax
+                max = mass[counter];
                 numMax = counter;
             }
 
             else if (mass[counter] < min) {
-                min = mass[counter];  // Минимальное число число записывается в min, а индекс в numMin
+                min = mass[counter];
                 numMin = counter;
             }
 
             counter++;
         }
 
-//        System.out.println(Arrays.toString(mass));
-//        System.out.println("Максимальный элемент массива: " + max + " Индекс: " + numMax);
-//        System.out.println("Минимальный элемент массива: " + min + " Индекс: " + numMin);
+        System.out.println(Arrays.toString(mass));
+        System.out.println("Максимальный элемент массива: " + max + " Индекс №: " + numMax);
+        System.out.println("Минимальный элемент массива: " + min + " Индекс №: " + numMin);
 
         int temp = mass[numMin];
-        mass[numMin] = mass[numMax]; // Минимальное значение в temp, максимальное копируем в минимальн, из temp в максимальн
+        mass[numMin] = mass[numMax];
         mass[numMax] = temp;
 
 
-//        System.out.println(Arrays.toString(mass));
+        System.out.println("---------- \nМассив, после перестановки элементов: \n" + Arrays.toString(mass));
 
+        counter = 0;
+        min = 0;
+        max = 0;
+        while (counter < mass.length) {
+            if (mass[counter] > max) {
+                max = mass[counter];
+                numMax = counter;
+            }
+
+            else if (mass[counter] < min) {
+                min = mass[counter];
+                numMin = counter;
+            }
+
+            counter++;
+        }
+
+        System.out.println("Максимальный элемент массива после перестановки: " + max + " Индекс №: " + numMax);
+        System.out.println("Минимальный элемент массива после перестановки: " + min + " Индекс №: " + numMin);
     }
 }
+
