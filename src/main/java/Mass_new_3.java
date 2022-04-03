448f647956b89008eca6b98b6b0b5a4bd899ae38
UselessMaxon <natasha.maxsim@yandex.ru>
2022-04-03 12:52:47 +0400

    3 задание с комментами

--- src/main/java/Mass_new_3.java
+++ src/main/java/Mass_new_3.java
@@ -29,16 +29,35 @@ public class Mass_new_3 {
             counter++;
         }
 
-//        System.out.println(Arrays.toString(mass));
-//        System.out.println("Максимальный элемент массива: " + max + " Индекс: " + numMax);
-//        System.out.println("Минимальный элемент массива: " + min + " Индекс: " + numMin);
+        System.out.println(Arrays.toString(mass));
+        System.out.println("Максимальный элемент массива: " + max + " Индекс №: " + numMax);
+        System.out.println("Минимальный элемент массива: " + min + " Индекс №: " + numMin);
 
         int temp = mass[numMin];
         mass[numMin] = mass[numMax]; // Минимальное значение в temp, максимальное копируем в минимальн, из temp в максимальн
         mass[numMax] = temp;
 
 
-//        System.out.println(Arrays.toString(mass));
+        System.out.println("---------- \nМассив, после перестановки элементов: \n" + Arrays.toString(mass));
 
+        counter = 0;
+        min = 0;
+        max = 0;
+        while (counter < mass.length) {
+            if (mass[counter] > max) {
+                max = mass[counter];   // Максимальное число записывается в max, а индекс в numMax
+                numMax = counter;
+            }
+
+            else if (mass[counter] < min) {
+                min = mass[counter];  // Минимальное число число записывается в min, а индекс в numMin
+                numMin = counter;
+            }
+
+            counter++;
+        }
+
+        System.out.println("Максимальный элемент массива после перестановки: " + max + " Индекс №: " + numMax);
+        System.out.println("Минимальный элемент массива после перестановки: " + min + " Индекс №: " + numMin);
     }
 }
